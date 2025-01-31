d3560e59206f7f96bb30ba959e922ed18ae68497
Tushar Chakravarti <tusarchakravarti@gmail.com>
2025-02-01 03:02:53 +0600

    Get Pawsome News Added. Responsive: Okay

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,34 +8,5 @@ class="mx-auto px-4 sm:px-6 lg:px-8 xl:px-10 2xl:px-12 max-w-7xl"
 
 max-sm:-mt-[180px] lg:-mt-[120px] left-[320px]
 
-<!--First Card-->
-          <div class="bg-gradient-to-r from-[#F8CCFB] to-[#ACD2FD] rounded-xl p-9 lg:w-8/12">
-            <!--Title-->
-            <div class="relative lg:my-32 ml-7">
-              <h5 class="font-sora text-xl max-sm:hidden">Under Price</h5>
-              <h1 class="font-ADLaM max-sm:text-xl text-3xl">Pet Grooming Mitt</h1>
-              <p class="font-ADLaM text-white text-2xl max-sm:hidden">(Ease Hair Removal)</p>
-              <h1 class="font-ADLaM max-sm:text-4xl max-sm:mb-48 text-white text-6xl">$7.99</h1>
-            </div>
-            <!--Image-->
-            <div class="absolute max-sm:-mt-[50px] max-sm:left[-200px] lg:-mt-[450px] left-[530px]">
-              <img class="max-sm:w-11/12 lg:w-full" src="./c1-assets/cute-dog-mid.png" alt="">
-            </div>
-          </div>
-          <!--Second Card-->
-          <div class="bg-gradient-to-r from-gray-100 to-yellow-200 rounded-xl p-9 w-4/12">
-            <!--Title-->
-            <div class="relative">
-              <h5 class="font-sora text-xl max-sm:hidden">Under Price</h5>
-              <h1 class="font-ADLaM max-sm:text-xl text-3xl">T-Shirt for Dogs</h1>
-              <p class="font-ADLaM text-white text-2xl max-sm:hidden">(Ease Hair Removal)</p>
-              <h1 class="font-ADLaM max-sm:text-4xl max-sm:mb-48 text-yellow-300 text-6xl">$7.99</h1>
-            </div>
-            <!--Image-->
-            <div class="absolute -mt-[250px] left-[240px]">
-              <img class="w-10/12" src="./c1-assets/cute-pet-mid.png" alt="">
-            </div>
-          </div>
-          </div>
 
 */
